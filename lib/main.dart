cb84c0f08eeac723a319bc9ac83f6ce6ca1ae51c
Soumitra Mandal <soumitramandal1999@gmail.com>
2022-02-05 23:34:02 +0530

    tabs added

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,66 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  static const _pages = <Widget>[
+    Center(
+      child: Icon(
+        Icons.message,
+        size: 40,
+      ),
+    ),
+    Center(
+      child: Icon(
+        Icons.add,
+        size: 40,
+      ),
+    ),
+    Center(
+      child: Icon(
+        Icons.alarm,
+        size: 40,
+      ),
+    )
+  ];
+
+  static const _tabs = <Tab>[
+    Tab(
+      icon: Icon(Icons.message),
+      text: 'Msg',
+    ),
+    Tab(
+      icon: Icon(Icons.add),
+      text: 'Add',
+    ),
+    Tab(
+      icon: Icon(Icons.alarm),
+      text: 'Alarm',
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      length: _pages.length,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,50 +71,19 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
+          elevation: 1,
+          backgroundColor: Colors.blue[600],
         ),
-        drawer: Drawer(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              Text('Row 1'),
-              Text("Row 2"),
-              Text('Row 3'),
-            ],
+        body: TabBarView(
+          children: _pages,
+          controller: _tabController,
+        ),
+        bottomNavigationBar: Material(
+          color: Colors.blue[600],
+          child: TabBar(
+            tabs: _tabs,
+            controller: _tabController,
           ),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text("Item 1"),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text("Item 1"),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text("Item 1"),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
